ea644dcb207aa94fe22a1eb6e5b5922e865f21b6
morgan <morgankclark@gmail.com>
2019-11-05 19:21:51 -0800

    Some changes before my flight. Don't fuck it up.

--- Booking/views.py
+++ Booking/views.py
@@ -16,20 +16,20 @@ def index():
 
 ######
 
-    # creating an apartments
-    cancun_apartment = Apartments('OCEANFRONT - CASA MAYA PLAYA - BEST LOCATION IN THE FABULOUS HOTEL ZONE', '383', 'Casatotalmente.webp', 'Cancun', '2', '2', '3', '700')
-
-    # adding the new apartment to the database
-    db.session.add(cancun_apartment)
-    db.session.commit()
-
-    # creating features for the apartment
-
-    cancun_apartment_feature1 = ApartmentFeatures('Children Welcome', cancun_apartment.id, 'Featured')
-    cancun_apartment_feature2 = ApartmentFeatures('TV', cancun_apartment.id, 'Featured')
-
-    db.session.add_all([cancun_apartment_feature1, cancun_apartment_feature2])
-    db.session.commit()
+    # # creating an apartments
+    # cancun_apartment = Apartments('OCEANFRONT - CASA MAYA PLAYA - BEST LOCATION IN THE FABULOUS HOTEL ZONE', '383', 'Casatotalmente.webp', 'Cancun', '2', '2', '3', '700')
+    #
+    # # adding the new apartment to the database
+    # db.session.add(cancun_apartment)
+    # db.session.commit()
+    #
+    # # creating features for the apartment
+    #
+    # cancun_apartment_feature1 = ApartmentFeatures('Children Welcome', cancun_apartment.id, 'Featured')
+    # cancun_apartment_feature2 = ApartmentFeatures('TV', cancun_apartment.id, 'Featured')
+    #
+    # db.session.add_all([cancun_apartment_feature1, cancun_apartment_feature2])
+    # db.session.commit()
 
 #######
 
@@ -47,6 +47,13 @@ def search_results():
     return render_template('search_results.html', apartments=search_result_apartments, location=location)
 
 
-@booking_app.route('/search_details')
+@booking_app.route('/search_details', methods=['GET', 'POST'])
 def search_details():
-    return render_template('search_details.html')
+
+    apartment = db.session.query(Apartments.name, Apartments.price, Apartments.image, Apartments.baths, Apartments.bedrooms, Apartments.sleeps).filter(Apartments.id == '12')
+    apartment_details_featured = db.session.query(ApartmentFeatures.feature_name).order_by(ApartmentFeatures.id).filter(ApartmentFeatures.apartment_id == '12', ApartmentFeatures.category == 'Featured')
+    apartment_details_safety_features = db.session.query(ApartmentFeatures.feature_name).order_by(ApartmentFeatures.id).filter(ApartmentFeatures.apartment_id == '12', ApartmentFeatures.category == 'Safety Features')
+    apartment_details_general = db.session.query(ApartmentFeatures.feature_name).order_by(ApartmentFeatures.id).filter(ApartmentFeatures.apartment_id == '12', ApartmentFeatures.category == 'General')
+
+
+    return render_template('search_details.html', apartment=apartment, apartment_details_featured=apartment_details_featured)
--- test_db_recordcreate.py
+++ test_db_recordcreate.py
@@ -4,15 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
-db = SQLAlchemy()
-app = create_app()
-Migrate(app,db)
-
-
-
-# creating an apartments
-cancun_apartment = Apartments('OCEANFRONT - CASA MAYA PLAYA - BEST LOCATION IN THE FABULOUS HOTEL ZONE', '383', 'Casatotalmente.webp', 'Cancun', '2', '2', '3', '700')
-
-# adding the new apartment to the database
-db.session.add_all(cancun_apartment)
-db.session.commit()
+# db = SQLAlchemy()
+# app = create_app()
+# Migrate(app,db)
+#
+#
+#
+# # creating an apartments
+# cancun_apartment = Apartments('OCEANFRONT - CASA MAYA PLAYA - BEST LOCATION IN THE FABULOUS HOTEL ZONE', '383', 'Casatotalmente.webp', 'Cancun', '2', '2', '3', '700')
+#
+# # adding the new apartment to the database
+# db.session.add_all(cancun_apartment)
+# db.session.commit()
